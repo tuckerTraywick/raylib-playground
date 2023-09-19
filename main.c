11f3fce6cfa7252724d6caaefc06725d0fc79ee9
Tucker Traywick <tucker.traywick@gmail.com>
2023-09-19 02:11:19 -0400

    Deleted some stuff.

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
     printf("Hello, world!\n");
+    printf("Goodbye, world.\n");
+    int a;
+    free(&a);
     return 0;
 }
